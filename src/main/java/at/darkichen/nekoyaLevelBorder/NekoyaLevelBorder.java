29d81a5425989092fa1d1c075e9a79643f0be286
Darkichen <darkichen@gmx.at>
2026-05-12 15:14:07 +0200

    Initial Commit

---
+++ src/main/java/at/darkichen/nekoyaLevelBorder/NekoyaLevelBorder.java
@@ -0,0 +1,16 @@
+package at.darkichen.nekoyaLevelBorder;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NekoyaLevelBorder extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
